10aca03f08b01e7afe8a1bfa643b30660bf03fa6
Jeff Hurst <strangehurst@users.noreply.github.com>
2026-05-26 15:57:21 -0500

    restore using PNML: LabelParser, ToolParser

--- PNML/src/Parser/Parser.jl
+++ PNML/src/Parser/Parser.jl
@@ -3,7 +3,7 @@ Parser module of PNML.
 
 Exports: [`@xml_str`(@ref)], [`XMLNode`](@ref), [`pnmlmodel`](@ref), [`xmlnode`](@ref).
 
-See [`LabelParser`](@ref), (`ToolParser`)(@ref).
+See [`LabelParser`](@ref), [`ToolParser`](@ref).
 """
 module Parser
 import AutoHashEquals: @auto_hash_equals
@@ -11,7 +11,8 @@ import Base: eltype
 import EzXML
 import Multisets: Multisets, Multiset
 import OrderedCollections: LittleDict, OrderedDict, OrderedSet, freeze
-import PNML: adjacent_place, netdata, refid, sortdefinition, sortelements, sortref, tag, verify!
+import PNML:
+    adjacent_place, netdata, refid, sortdefinition, sortelements, sortref, tag, verify!
 import XMLDict
 
 using ..Declarations
@@ -19,10 +20,10 @@ using ..Expressions
 using ..IDRegistrys
 using ..Labels
 using ..Labels: validate_toolinfos
+using ..PnmlGraphics
 using ..PnmlTypes
 using ..Sorts
 using ..Sorts: equalSorts, sorts
-using ..PnmlGraphics
 using Base: isempty, length
 using DocStringExtensions
 using Logging
@@ -31,22 +32,19 @@ using Moshi.Data: @data, is_data_type, isa_variant
 using Moshi.Match: @match
 using NamedTupleTools
 using PNML
-using PNML: AnyElement, BooleanConstant, CONFIG, Coordinate, D, DeclDict,
-    FEConstant, MalformedException, Maybe, MissingIDException, PnmlNetData, PnmlNetKeys,
-    XmlDictType, arbitrarysorts, arc_idset, arcdict, basis, coordinate_type,
-    decldict, feconstants,
-    fill_builtin_enabled_filters!, fill_builtin_labelparsers!, fill_builtin_sorts!,
-    fill_builtin_toolparsers!, fill_sort_tag!, has_arbitrarysort,
+using PNML: AnyElement, BooleanConstant, CONFIG, Coordinate, D, DeclDict, FEConstant,
+    LabelParser, MalformedException, Maybe, MissingIDException, PnmlNetData, PnmlNetKeys,
+    ToolParser, XmlDictType, arbitrarysorts, arc_idset, arcdict, basis, coordinate_type,
+    decldict, feconstants, fill_builtin_enabled_filters!, fill_builtin_labelparsers!,
+    fill_builtin_sorts!, fill_builtin_toolparsers!, fill_sort_tag!, has_arbitrarysort,
     has_feconstant, has_multisetsort, has_namedsort, has_partitionsort, has_place,
-    has_productsort, is_arbitrarysort, is_inhibitor, is_multisetsort,
-    is_namedsort, is_normal,
-    is_partitionsort, is_productsort, is_read, is_reset, is_usersort, multisetsorts,
-    namedoperators, namedsorts, netsets, number_value, operator, page_idset, pagedict,
-    partitionsorts, pid, place, place_idset, placedict, pntd_of, productsorts,
-    refplace_idset, refplace_idset, refplacedict, reftransition_idset, reftransitiondict,
-    registry_of, to_sort, toolinfos, transition_idset, transitiondict, value_type,
-    variabledecl, variabledecls, verify
-
+    has_productsort, is_arbitrarysort, is_inhibitor, is_multisetsort, is_namedsort,
+    is_normal, is_partitionsort, is_productsort, is_read, is_reset, is_usersort,
+    multisetsorts, namedoperators, namedsorts, netsets, number_value, operator, page_idset,
+    pagedict, partitionsorts, pid, place, place_idset, placedict, pntd_of, productsorts,
+    refplace_idset, refplacedict, reftransition_idset, reftransitiondict, registry_of,
+    to_sort, toolinfos, transition_idset, transitiondict, value_type, variabledecl,
+    variabledecls, verify
 using SciMLPublic: @public
 using TermInterface
 
