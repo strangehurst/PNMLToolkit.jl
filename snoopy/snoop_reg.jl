5cce5cd1e16bc444f7681336f296bb8144e083ee
Jeff Hurst <strangehurst@users.noreply.github.com>
2026-07-19 16:18:46 -0500

    update script to match newer snoopy

--- snoopy/snoop_reg.jl
+++ snoopy/snoop_reg.jl
@@ -1,38 +1,48 @@
 # SnoopCompile
 using SnoopCompileCore
+invs = @snoop_invalidations using PNML;
+using SnoopCompile, AbstractTrees
+trees = invalidation_trees(invs);
+tree = trees[1]
+sig, victim = tree.mt_backedges[end];
+sig
+victim
+print_tree(victim)
+invalidated(invs)
 
-invalidations = @snoopr begin
-    #!using PnmlIDRegistrys
 
-    tinf = @snoopi_deep begin
-        @with PNML.idregistry => registry() begin
-            register_id!(idregistry[], :p)
-            register_id!(idregistry[], :p)
-            !isregistered(idregistry[], "p")
-            !isregistered(idregistry[], :p)
-        end
-    end
-end
+# invalidations = @snoopr begin
+#     #!using PnmlIDRegistrys
 
-using SnoopCompile
+#     tinf = @snoopi_deep begin
+#         @with PNML.idregistry => registry() begin
+#             register_id!(idregistry[], :p)
+#             register_id!(idregistry[], :p)
+#             !isregistered(idregistry[], "p")
+#             !isregistered(idregistry[], :p)
+#         end
+#     end
+# end
 
-trees = SnoopCompile.invalidation_trees(invalidations);
-staletrees = precompile_blockers(trees, tinf)
+# using SnoopCompile
 
-@show length(SnoopCompile.uinvalidated(invalidations)) # show total invalidations
+# trees = SnoopCompile.invalidation_trees(invalidations);
+# staletrees = precompile_blockers(trees, tinf)
 
-if !isempty(trees)
-    show(trees[end]) # show the most invalidating method
+# @show length(SnoopCompile.uinvalidated(invalidations)) # show total invalidations
 
-    # Count number of children (number of invalidations per invalidated method)
-    n_invalidations = map(SnoopCompile.countchildren, trees)
+# if !isempty(trees)
+#     show(trees[end]) # show the most invalidating method
 
-    import Plots
-    Plots.plot(
-        1:length(trees),
-        n_invalidations;
-        markershape=:circle,
-        xlabel="i-th method invalidation",
-        label="Number of children per method invalidations"
-    )
-end
+#     # Count number of children (number of invalidations per invalidated method)
+#     n_invalidations = map(SnoopCompile.countchildren, trees)
+
+#     import Plots
+#     Plots.plot(
+#         1:length(trees),
+#         n_invalidations;
+#         markershape=:circle,
+#         xlabel="i-th method invalidation",
+#         label="Number of children per method invalidations"
+#     )
+# end
