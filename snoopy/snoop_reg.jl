19df6939326fd01d3c5bb2907d19f8968cf3f9f7
Jeff Hurst <strangehurst@users.noreply.github.com>
2026-07-21 14:59:12 -0500

    comments

--- snoopy/snoop_reg.jl
+++ snoopy/snoop_reg.jl
@@ -2,32 +2,18 @@
 using SnoopCompileCore
 invs = @snoop_invalidations using PNML;
 using SnoopCompile, AbstractTrees
-trees = invalidation_trees(invs);
+trees = invalidation_trees(invs)
 tree = trees[1]
 sig, victim = tree.mt_backedges[end];
 sig
 victim
 print_tree(victim)
-invalidated(invs)
+#invalidated(invs)
 
 
-# invalidations = @snoopr begin
-#     #!using PnmlIDRegistrys
 
-#     tinf = @snoopi_deep begin
-#         @with PNML.idregistry => registry() begin
-#             register_id!(idregistry[], :p)
-#             register_id!(idregistry[], :p)
-#             !isregistered(idregistry[], "p")
-#             !isregistered(idregistry[], :p)
-#         end
-#     end
-# end
 
-# using SnoopCompile
 
-# trees = SnoopCompile.invalidation_trees(invalidations);
-# staletrees = precompile_blockers(trees, tinf)
 
 # @show length(SnoopCompile.uinvalidated(invalidations)) # show total invalidations
 
