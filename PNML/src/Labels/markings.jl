fb7878c0ef1e575a7401986a1e8705be56f9123a
Jeff Hurst <strangehurst@users.noreply.github.com>
2026-08-22 08:48:42 -0500

    formatting change

--- PNML/src/Labels/markings.jl
+++ PNML/src/Labels/markings.jl
@@ -19,11 +19,11 @@ end
 import Base: ==, hash
 function Base.:(==)(a::Marking, b::Marking)
     return (a.term == b.term) &&
-        (a.text == b.text) &&
-        (a.graphics == b.graphics) &&
-        (a.toolspecinfos == b.toolspecinfos) &&
-        (a.net == b.net) &&
-        (a.place == b.place)
+            (a.text == b.text) &&
+            (a.graphics == b.graphics) &&
+            (a.toolspecinfos == b.toolspecinfos) &&
+            (a.net == b.net) &&
+            (a.place == b.place)
 end
 
 function Base.hash(u::Marking, h::UInt)
