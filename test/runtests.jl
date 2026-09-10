f7b565d1508a83c42fe18b858bf365de51973973
Jeff Hurst <strangehurst@users.noreply.github.com>
2026-09-09 21:32:02 -0500

    add ARGS

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,7 @@
 using Test, SafeTestsets, Pkg
 
+@show ARGS
+isempty(ARGS) && push!(ARGS, "ALL")
 
-Pkg.test("PNML")
-Pkg.test("PNet")
+Pkg.test("PNML"; test_args=ARGS)
+Pkg.test("PNet"; test_args=ARGS)
